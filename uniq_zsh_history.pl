d41a5359530febddbfe65adb5f515c5ab95486b4
Hirokazu Chiba <hchiba1@gmail.com>
2021-01-24 14:26:16 +0900

    refactor uniq_zsh_history.pl

--- uniq_zsh_history.pl
+++ uniq_zsh_history.pl
@@ -1,23 +1,17 @@
 #!/usr/bin/perl -w
 use strict;
 use File::Basename;
-use Getopt::Std;
 my $PROGRAM = basename $0;
 my $USAGE=
 "Usage: $PROGRAM
 ";
 
-my %OPT;
-getopts('', \%OPT);
-
 !@ARGV && -t and die $USAGE;
 my %HASH = ();
 while (<>) {
-    chomp;
-    if (/^(\S+ \S+)  (.*)$/) {
+    if (/^(\S+ \S+)  (.*)/) {
         my ($date, $command) = ($1, $2);
-        if ($HASH{$command}) {
-        } else {
+        if (!$HASH{$command}) {
             $HASH{$command} = 1;
             print "$date  $command\n";
         }
