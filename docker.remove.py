00258027a4e7f1fa7c8d96a2bf5a83bf3708bde5
Hirokazu Chiba <hchiba1@gmail.com>
2022-02-23 06:47:31 +0900

    Add docker.remove.py

---
+++ docker.remove.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import argparse
+import subprocess
+import sys
+
+parser = argparse.ArgumentParser(description='Remove docker container or image')
+parser.add_argument('name', help='container or image')
+args = parser.parse_args()
+
+ret1 = subprocess.run([ 'docker', 'rm', args.name ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+if ret1.returncode != 0:
+    ret2 = subprocess.run([ 'docker', 'rmi', args.name ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if ret2.returncode != 0:
+        print('cannot remove', file=sys.stderr)
+    else:
+        print('removed image', file=sys.stderr)
+else:
+    print(ret1.stdout.decode())
+    print(ret1.stderr.decode(), file=sys.stderr)
