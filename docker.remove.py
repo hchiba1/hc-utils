cc329d0fddffadf16736af4d9721e0a53949e2e2
Hirokazu Chiba <hchiba1@gmail.com>
2022-04-15 07:51:42 +0900

    Refactor docker.remove.py

--- docker.remove.py
+++ docker.remove.py
@@ -9,12 +9,12 @@ args = parser.parse_args()
 
 ret1 = subprocess.run([ 'docker', 'rm', args.name ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-if ret1.returncode != 0:
+if ret1.returncode == 0:
+    print(ret1.stdout.decode())
+    print(ret1.stderr.decode(), file=sys.stderr)
+else:
     ret2 = subprocess.run([ 'docker', 'rmi', args.name ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if ret2.returncode != 0:
         print('cannot remove', file=sys.stderr)
     else:
         print('removed image', file=sys.stderr)
-else:
-    print(ret1.stdout.decode())
-    print(ret1.stderr.decode(), file=sys.stderr)
