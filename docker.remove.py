1223d15f126feeb66aa7245000db25428c027512
Hirokazu Chiba <hchiba1@gmail.com>
2022-04-15 12:29:23 +0900

    Update docker.remove.py

--- docker.remove.py
+++ docker.remove.py
@@ -12,9 +12,10 @@ ret1 = subprocess.run([ 'docker', 'rm', args.name ], stdout=subprocess.PIPE, std
 if ret1.returncode == 0:
     print(ret1.stdout.decode())
     print(ret1.stderr.decode(), file=sys.stderr)
+    sys.exit()
+
+ret2 = subprocess.run([ 'docker', 'rmi', args.name ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+if ret2.returncode != 0:
+    print('cannot remove', file=sys.stderr)
 else:
-    ret2 = subprocess.run([ 'docker', 'rmi', args.name ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    if ret2.returncode != 0:
-        print('cannot remove', file=sys.stderr)
-    else:
-        print('removed image', file=sys.stderr)
+    print('removed image', file=sys.stderr)
