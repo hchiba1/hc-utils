164983535e5a163d866cb091aa2cb4c474e96685
Hirokazu Chiba <hchiba1@gmail.com>
2022-04-17 19:22:26 +0900

    Modify docker.remove.py to accept multiple files

--- docker.remove.py
+++ docker.remove.py
@@ -4,21 +4,22 @@ import subprocess
 import sys
 
 parser = argparse.ArgumentParser(description='Remove docker container or image')
-parser.add_argument('name', help='container or image')
+parser.add_argument('name', nargs='+', help='container or image')
 args = parser.parse_args()
 
-ret1 = subprocess.run([ 'docker', 'rm', args.name ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-if ret1.returncode == 0:
-    print('removed container', file=sys.stderr)
-    sys.exit()
+def remove(id):
+    ret1 = subprocess.run([ 'docker', 'rm', id ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if ret1.returncode == 0:
+        print(f'removed container {id}', file=sys.stderr)
+        return
 
-ret2 = subprocess.run([ 'docker', 'rmi', args.name ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-if ret2.returncode == 0:
-    print('removed image', file=sys.stderr)
-    sys.exit()
+    ret2 = subprocess.run([ 'docker', 'rmi', id ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if ret2.returncode == 0:
+        print(f'removed image {id}', file=sys.stderr)
+        return
 
-print('cannot remove', file=sys.stderr)
-print('rm:', ret1.stdout.decode())
-print('rm err:', ret1.stderr.decode(), file=sys.stderr)
-print('rmi:', ret2.stdout.decode())
-print('rmi err:', ret2.stderr.decode(), file=sys.stderr)
+    print(ret1.stderr.decode(), end='', file=sys.stderr)
+    print(ret2.stderr.decode(), end='', file=sys.stderr)
+
+for id in args.name:
+    remove(id)
