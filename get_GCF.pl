8fef04c20388675341e7389c3f731bf69f4adfe4
Hirokazu Chiba <hchiba1@gmail.com>
2024-04-13 08:55:27 +0900

    Modify get_GCF.pl to check update

--- get_GCF.pl
+++ get_GCF.pl
@@ -2,30 +2,96 @@
 use strict;
 use File::Basename;
 use Getopt::Std;
+use HTTP::Date 'str2time', 'time2iso';
 my $PROGRAM = basename $0;
 my $USAGE=
 "Usage: $PROGRAM URL
--d: directory contents
 ";
 
-my %OPT;
-getopts('d', \%OPT);
+my $COMMAND = "curl --max-time 100000 -LfsS";
 
+my %OPT;
+getopts('', \%OPT);
+
+### get URL
 if (!@ARGV) {
     print STDERR $USAGE;
     exit 1;
 }
 my ($URL) = @ARGV;
-
 $URL =~ s/^https:\/\///;
+
 if ($URL =~ /(GCF_\S+)$/) {
-    my $gcf = $1;
-    if ($OPT{d}) {
-        system "get $URL/";
-    } else {
-        system "get -f $URL/${gcf}_protein.faa.gz";
+    my $local_name = "${1}_protein.faa.gz";
+    if (-f $local_name) {
+        check_update($local_name);
     }
 } else {
     print STDERR "ERROR: $URL is not a valid GCF\n";
     exit 1;
+}
+
+################################################################################
+### Function ###################################################################
+################################################################################
+
+sub check_update {
+    my ($local_name) = @_;
+
+    my $local_day = get_local_day($local_name);
+    my $local_size = get_local_size($local_name);
+
+    my @list = `$COMMAND $URL/`;
+    chomp(@list);
+    for my $line (@list) {
+        if ($line =~ /^(.*?) +(\d+) +(\S+) +(\S+) +(\d+) (\S+ +\S+ +\S+) (.*)/) {
+            my ($perm, $num, $group, $user, $size, $date, $name) = ($1, $2, $3, $4, $5, $6, $7);
+            if ($local_name eq $name) {
+                my $day = get_day($date);
+                if ($local_day eq $day && $local_size eq $size) {
+                    print "Already updated: $local_name\n";
+                } else {
+                    if ($local_day ne $day) {
+                        print "Day is different: $local_day => new $day $local_name\n";
+                    } elsif ($local_size ne $size) {
+                        print "Size is different: $local_size => new $size $local_name\n";
+                    }
+                    system "$COMMAND -OR $URL/$local_name";
+                }
+            }
+        }
+    }
+}
+
+sub get_day {
+    my ($date) = @_;
+
+    my $time = time2iso(str2time($date, "GMT"));
+    $time =~ s/:00$//;
+    if ($time =~ /^(\S+) \S+$/) {
+        return $1;
+    } else {
+        die $time;
+    }
+}
+
+sub get_local_day {
+    my ($file) = @_;
+
+    my @stat = stat $file;
+    my $time = time2iso($stat[9]);
+
+    if ($time =~ /^(\S+) \S+$/) {
+        return $1;
+    } else {
+        die $time;
+    }
+}
+
+sub get_local_size {
+    my ($file) = @_;
+
+    my @stat = stat $file;
+
+    return $stat[7];
 }
